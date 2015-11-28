5f4a020e73f6edcf273503ebbace7730d4023920
kerstin <kerstin@EN-SS-AKEF73-A.local>
2015-11-28 00:15:12 -0500

    fixed score writing

--- dblp_test.py
+++ dblp_test.py
@@ -10,7 +10,9 @@ my_test = redspline.Spectral(data, n)
 
 my_test.fit(interpolate.UnivariateSpline, k)
 my_test.plot()
-my_test.score()
+
+with open('dblp_score.txt', 'w') as score:
+    score.write(str(my_test.score())
 
 with open('dblp_dpline.pickle', 'wb') as handle:
   pickle.dump(my_test, handle)
