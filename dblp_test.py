586203b883fb64e5eeca8a84e2b150df8b956e5a
kerstin <kerstin@dhcp-uris-2883.redrover.cornell.edu>
2015-12-09 14:31:13 -0500

    added a markdown writer, fixed fit bug

--- dblp_test.py
+++ dblp_test.py
@@ -3,7 +3,8 @@ from scipy import interpolate
 import pickle
 
 data = "snap_dblp.txt"
-n = 317080
+#n = 317080
+n = 12563
 k = 126
 
 my_test = redspline.Spectral(data, n)
@@ -11,8 +12,10 @@ my_test = redspline.Spectral(data, n)
 my_test.fit(interpolate.UnivariateSpline, k)
 my_test.plot()
 
-with open('dblp_score.txt', 'w') as score:
-    score.write(str(my_test.score()))
+#with open('dblp_score.txt', 'w') as score:
+#    score.write(str(my_test.score()))
 
-with open('dblp_dpline.pickle', 'wb') as handle:
-    pickle.dump(my_test, handle)
+#with open('dblp_dpline.pickle', 'wb') as handle:
+#    pickle.dump(my_test, handle)
+
+my_test.document()
--- redspline.py
+++ redspline.py
@@ -30,14 +30,31 @@ class Spectral:
         self.vertices = range(n)
         self.adjList = adj_from_listfile(file_name, n)
         print "adjacency matrix loaded"
+
+    # def setRep(self, *repList):
+    #     if not set(repList).issubset(set('A', 'nA', 'L', 'nL')):
+    #         print "INPUT ERROR"
+    #     elif 'A' in repList:
+    #         self.A = self.adjList
+    #     elif 'nA' in repList:
+    #         pass
+    #     elif 'L' in repList:
+    #         pass
+    #     elif 'nL' in repList:
+    #         pass
+
+#laplacianMatrix()
+#normalisedLaplacianSym()
+#row_sums = A.sum(axis=1)
+#new_matrix = A / row_sums[:, numpy.newaxis]
     
-    def fit(self, function, k=False):
+    def fit(self, function, source=False, target=False, split=False, k=False):
         """
             Sets up the training and test set and fits the spectral
             learning function to it.
         """
 
-        self.train, self.test = splitGraph(self.adjList)
+        self.train, self.test = splitGraph(self.adjList, split)
         print "train test split created"
         cc = self.train.findConnectedComponents()[0]
         print "connected component found"
@@ -61,7 +78,7 @@ class Spectral:
         """
             Produces a plot of eigenvalues against the Frobenius norm fitting
         """
-        plot_against(self.rrVal, self.targetTrans.diagonal() , self.fitted, self.name)
+        self.plotFile = plot_against(self.rrVal, self.targetTrans.diagonal() , self.fitted, self.name)
         print "plot produced"
         
     def predict(self, graph=False):
@@ -90,38 +107,98 @@ class Spectral:
         print "scoring tallied"
         return pearsonr( predicted, true)
 
+    def document(self):
+        with open(self.name + ".md", 'a') as md:
+            md.write(mdText(self))
+
+
 class Bootstral(Spectral):
 
     def __init__(self, spectral_obj, i, n):
         self.fileName = spectral_obj.fileName
         self.name = spectral_obj.name + "_" + str(i)
         self.vertices = random.sample(spectral_obj.vertices, n)
+        print "sample chosen"
         self.adjList = spectral_obj.adjList.subgraph(self.vertices)
+        print "graph constructed"
 
 def projRot(omatrix, cmatrix):
     return np.dot(np.transpose(omatrix), np.dot(cmatrix, omatrix))
 #interpolate.UnivariateSpline(self.rrVal, self.targetTrans.diagonal())
-        
+    
+def mdText(obj):
+    return "#" + obj.name + ": " + "source" +" -> " + "target" + ":" + "fit" + " \n" + \
+            "Time split: " + "timeSplit" + "\n" + \
+            "##Newtowrk Statistics \n " + "networkStats" + "\n" + \
+            "Pickled model" + "pickled" + "\n" + \
+            "Score" + "score" + "\n" + \
+            "![](" + obj.plotFile + ")" + "\n"
+
+def getList(data, skip=4):    
+    with open(data, "r") as big_file:
+        i = 0
+        myData = big_file.readlines()
+    adj1 = []
+    adj2 = []
+    for each in myData[skip:]:
+        i,j = each.strip('\n').split('\t')
+        adj1.append(int(i))
+        adj2.append(int(j))
+    print "data retrieved"
+    return adj1, adj2
+
+def getAdjDict(data, skip=4):    
+    with open(data, "r") as big_file:
+        i = 0
+        myData = big_file.readlines()
+    adjDict = {}
+    for each in myData[skip:]:
+        i,j = each.strip('\n').split('\t')
+        if i in adjDict:
+            adjDict[i].append[j]
+        else:
+            adjDict[i] = [j]
+        if j in adjDict:
+            adjDict[j].append[i]
+        else:
+            adjDict[j] = [i]
+    print "data retrieved"
+    return adjDict
 
 def adj_from_listfile(adj_list, n, vertices=False, skip=4):
     if not vertices:
         vertices = range(n)
     adj = SparseGraph(len(vertices))
-    with open(adj_list, 'r') as a:
-        k = 0
-        for line in a:
-            if k < skip:
-                k+=1
-            else:
-                i, j = line.split()
-                i = int(i)
-                j = int(j)
-                if i in vertices and j in vertices:
-                    adj[i, j] = 1
-                    adj[j, i] = 1
+    print "sparse graph created"
+    adj1, adj2 = getList(adj_list, skip)
+    for i,j in zip(adj1,adj2):
+        if i >= n:
+            break
+        if j >= n:
+            pass
+        elif i in vertices and j in vertices:
+            adj[i, j] = 1
+    print "adj list constructed"
     return adj
 
-def splitGraph(graph, p=1/float(3)):
+def arrange(adj1, adj2):
+    index = 0
+    for i,j in zip(adj1,adj2):
+        if i > j:
+            adj1[index] = j
+            adj2[index] = i
+        index+=1
+    return adj1, adj2
+
+def breadthFirstSearch(adjDict):
+    stack = []
+    def explore(i):
+        stack = set(stack + adjDict[i])
+
+
+
+
+def splitGraph(graph, random=True ,p=1/float(3)):
     all_edges = graph.getAllEdges()
     vertices =  graph.vlist
     inc = bernoulli.rvs(p, size = len(all_edges))
@@ -132,8 +209,10 @@ def splitGraph(graph, p=1/float(3)):
     return train, test
 
 def plot_against(D, B, fit, name="fit"):
+    fig_name = name + "_" + str(datetime.date.today()) + '.png'
     plt.plot(D, B, 'ro', ms=5)
     plt.plot(D, fit(D), 'g', lw=3)
     fit.set_smoothing_factor(0.5)
     plt.plot(D, fit(D), 'b', lw=3)
-    plt.savefig(name + "_" + str(datetime.date.today()) + '.png')
+    plt.savefig(fig_name)
+    return fig_name
